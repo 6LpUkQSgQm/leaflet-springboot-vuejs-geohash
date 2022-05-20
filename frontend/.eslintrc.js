e2a8254abd046651f537a923ed556f9502f8fc21
Julien Chapron <jc271200001@protonmail.com>
2022-05-20 10:08:10 +0200

    [codacy]  fix -> 'errors import module axios test'

--- frontend/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    "rules": {
-        "node/no-missing-import": ["error", {
-            "allowModules": ["vue"],
-            "resolvePaths": ["../frontend/node_modules/vue"],
-            "tryExtensions": [".js", ".json", ".node"]
-        }]
-    }
-};
--- frontend/src/components/http-common.js
+++ frontend/src/components/http-common.js
@@ -1,5 +1,5 @@
 /*eslint node/no-missing-import: "error" */
-import axios from "axios";
+import axios from "../../node_modules/axios";
 
 export const AXIOS = axios.create({
   baseURL: "http://127.0.0.1:8093/graphql",
